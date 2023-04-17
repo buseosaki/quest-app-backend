8ccf26f09aeaea298b55cdf054a9dad7db1cc328
Zelal Buse Kizildag <zelalkizildag98@gmail.com>
2023-04-17 23:25:29 +0300

    comment and post entities changed

--- src/main/java/com/example/questapp/entities/Comment.java
+++ src/main/java/com/example/questapp/entities/Comment.java
@@ -1,9 +1,17 @@
 package com.example.questapp.entities;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import lombok.Data;
@@ -16,8 +24,13 @@ public class Comment {
 	
 	@Id
 	Long id;
-	Long postId;
-	Long userId;
+	Long postId; //burayı da sekillendir
+	
+	@ManyToOne(fetch = FetchType.LAZY)  //bir sürü postun tek bir userı olabilir 
+	@JoinColumn(name="user_id", nullable=false) //user id'yi post'a bağladık
+	@OnDelete(action = OnDeleteAction.CASCADE) //bir user silindiğinde postları da silinsin
+	@JsonIgnore
+	User user;
 
 	@Lob
 	@Column(columnDefinition = "text")
--- src/main/java/com/example/questapp/entities/Post.java
+++ src/main/java/com/example/questapp/entities/Post.java
@@ -1,9 +1,17 @@
 package com.example.questapp.entities;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 import lombok.Data;
@@ -14,7 +22,14 @@ import lombok.Data;
 public class Post {
 	@Id
 	Long id;
-	Long userId;
+	
+	
+	@ManyToOne(fetch = FetchType.LAZY)  //bir sürü postun tek bir userı olabilir 
+	@JoinColumn(name="user_id", nullable=false) //user id'yi post'a bağladık
+	@OnDelete(action = OnDeleteAction.CASCADE) //bir user silindiğinde postları da silinsin
+	@JsonIgnore
+	User user;
+	
 	String title;
 	@Lob
 	@Column(columnDefinition = "text")
