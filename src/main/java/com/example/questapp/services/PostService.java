a2bcc0d7fd92e076876188739354d932e30ae2cd
Zelal Buse Kizildag <zelalkizildag98@gmail.com>
2023-04-24 23:40:05 +0300

    postservice will be changed

--- src/main/java/com/example/questapp/services/PostService.java
+++ src/main/java/com/example/questapp/services/PostService.java
@@ -1,7 +1,13 @@
 package com.example.questapp.services;
 
-import org.springframework.stereotype.Service;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.questapp.entities.Post;
 import com.example.questapp.repos.PostRepository;
 
 @Service
@@ -12,4 +18,10 @@ public class PostService {
 	public PostService(PostRepository postRepository) {
 		this.postRepository = postRepository;
 	}
-}
+	
+	@GetMapping
+	public List<Post> getAllPosts(@RequestParam Optional <Long> userId){
+		return postService.getAllPosts(userId);
+		
+	}
+	}
