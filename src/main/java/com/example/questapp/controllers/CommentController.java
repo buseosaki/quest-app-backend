d9b12acd176a167ecc907be3058ff91eb941ed6f
Zelal Buse Kizildag <zelalkizildag98@gmail.com>
2023-05-02 23:52:42 +0300

    comment controller will change

--- src/main/java/com/example/questapp/controllers/CommentController.java
+++ src/main/java/com/example/questapp/controllers/CommentController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +27,11 @@ public class CommentController {
 	@GetMapping
 	public List<Comment> getAllComments(@RequestParam Optional <Long> userId){
 		return commentService.getAllComments(userId);
+		
+	}
+	
+	@PostMapping
+	public Comment createOneComment(@RequestBody CommentCreateRequest newCreateRequest) {
+		return commentService.createOneComment();
 	}
 }
